85d4da4b4553285f5257cace4e1c0a7f9e433fcf
GW <gwsounddsg@gmail.com>
2020-12-20 09:15:02 -0800

    add basic.cpp

---
+++ lib/basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten/bind.h>
+#endif
+
+void myPrint() {
+    std::cout << "Hello World!" << std::endl;
+}
+
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_BINDINGS(basicModule) {
+    function("myPrint", $myPrint);
+}
+#endif
